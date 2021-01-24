0a8fe3cc8f994979e6794fb412b12e1ae43a095f
codecheap2019 <webloopofficial@gmail.com>
2021-01-24 09:55:19 +0530

    Initial Commit

---
+++ admin/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function App() {
+  return(
+    <React.Fragment>Admin</React.Fragment>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('react-app')
+);
---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function App() {
+  return(
+    <React.Fragment>Customer</React.Fragment>
+  )
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('react-app')
+);
